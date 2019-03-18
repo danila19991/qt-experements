fd3754af0d2a03973f0bedb6b074dc6ae2e28c9d
Данила <guseffdanila@ya.ru>
2019-03-18 14:23:52 +0300

    add test class7

--- tests/test-runner.cpp
+++ tests/test-runner.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    freopen("testing.log", "w", stdout);
     QCoreApplication a(argc, argv);
     auto res = QTest::qExec(new test_smart, argc, argv);
     return res;
