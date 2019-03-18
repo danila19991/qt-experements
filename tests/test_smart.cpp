14ee8f1360b17862ebaf232d0b832e35737347ad
Данила <guseffdanila@ya.ru>
2019-03-18 14:53:37 +0300

    add test class12

--- tests/test_smart.cpp
+++ tests/test_smart.cpp
@@ -13,13 +13,13 @@ void test_smart::sum_data()
     QTest::addColumn<double>("second");
     QTest::addColumn<double>("result");
 
-    QTest::newRow("sum_data_1") << 1.l << 0.l << 1.l;
-    QTest::newRow("sum_data_2") << -1.l << 1.l << 0.l;
-    QTest::newRow("sum_data_3") << 4.l << 8.l << 12.l;
-    QTest::newRow("sum_data_4") << 0.l << 0.l << 0.l;
-    QTest::newRow("sum_data_5") << 1.l << 1.l << 2.l;
-    QTest::newRow("sum_data_6") << 0.l << 1.l << 1.l;
-    QTest::newRow("sum_data_7") << -10.l << -5.l << -15.l;
+    QTest::newRow("sum_data_1") << 1. << 0. << 1.;
+    QTest::newRow("sum_data_2") << -1. << 1. << 0.;
+    QTest::newRow("sum_data_3") << 4. << 8. << 12.;
+    QTest::newRow("sum_data_4") << 0. << 0. << 0.;
+    QTest::newRow("sum_data_5") << 1. << 1. << 2.;
+    QTest::newRow("sum_data_6") << 0. << 1. << 1.;
+    QTest::newRow("sum_data_7") << -10. << -5. << -15.;
     //QTest::newRow("sum_data_8") << -10.l << -5.l << -14.l;
 }
 
